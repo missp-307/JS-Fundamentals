25c5fdd3f74fff31bb05711fae49a6bf65762a5d
brian omusali <brianomusali@gmail.com>
2025-09-15 00:44:31 +0300

    added file 9

--- 9-add.js
+++ 9-add.js
@@ -2,7 +2,7 @@ function add(a, b) {
   return a + b;
 }
 
-const first = parseInt(process.argv[2]);
-const second = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-console.log(add(first, second));
+console.log(add(a, b));
